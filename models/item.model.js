fb1c8dd3f280d4a5659aa4f88745f7cf17d17eb3
Sourav Bishai <souravbishai58558@gmail.com>
2025-04-09 17:00:36 +0530

    reset repo then uplaod

---
+++ models/item.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  companyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Company",
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  salePrice: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'category',
+    required: true
+  },
+  details: {
+    type: String,
+  },
+  unit: {
+    type: [],
+    required: true
+  },
+  stock: {
+    type: [],
+    default: 0
+  },
+  alert: {
+    type: [],
+    default: 0
+  },
+  isDel: {
+    type: Boolean,
+    default: false
+  },
+  isTrash: {
+    type: Boolean,
+    default: false
+  }
+
+}, { timestamps: true });
+
+
+const itemModel = new mongoose.model("item", itemSchema);
+module.exports = itemModel;
